75d03c20c8659f4e3f36d942572caaff365221e3
Do Hyun <dksehgis@naver.com>
2021-02-09 15:09:05 +0900

    2021-02-09

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,7 @@ class Accomodation(models.Model):
     longitude = models.TextField()
     accomodation_type = models.TextField(null=True , blank = True)
     min_price = models.IntegerField(null=True , blank = True)
-    rating = models.FloatField((null=True , blank = True)
+    rating = models.FloatField(null=True , blank = True)
     general_review = models.TextField(null=True , blank = True)
     total_review_num = models.IntegerField(null=True , blank = True)
     image_link = models.URLField(null=True)
@@ -21,7 +21,9 @@ class Accomodation(models.Model):
     img_link_3 = models.URLField(null=True , blank = True)
     img_link_4 = models.URLField(null=True , blank = True)
     img_link_5 = models.URLField(null=True , blank = True)
-
+    owner_comment = models.TextField(null =True , blank=True)
+    
+    
     def __str__(self):
         return self.room_name
 
@@ -33,12 +35,15 @@ class Accomodation(models.Model):
         self.latitude = list[4]
         self.longitude = list[5]
         self.accomodation_type = list[6]
-        self.min_price = list[7]
-        self.rating = list[8]
-        self.general_review = list[9]
-        self.total_review_num = list[10]
+        if list[7] != " "  and list[7] != None:  self.min_price = list[7]
+         
+        if list[8] != " "  and list[8] != None: self.rating = list[8]
+        if list[9] != " "  and list[9] != None: self.general_review = list[9]
+        if list[10] != " " and list[10] != None: 
+            self.total_review_num = int(list[10])
         self.image_link = list[11]
         self.img_link_2 = list[12]
         self.img_link_3 = list[13]
         self.img_link_4 = list[14]
         self.img_link_5 = list[15]
+        if list[16] != " ": self.owner_comment = list[16]
