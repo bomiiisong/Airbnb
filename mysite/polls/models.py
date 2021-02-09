9611b4e4bacf20b9a3952139b25520b865f17e0c
Do Hyun <dksehgis@naver.com>
2021-02-09 13:48:22 +0900

    2021-02-09

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,8 +10,16 @@ class Accomodation(models.Model):
     location = models.TextField()
     latitude = models.TextField()
     longitude = models.TextField()
-    link = models.URLField()
-    image_link = models.URLField()
+    accomodation_type = models.TextField(null=True)
+    min_price = models.IntegerField(null=True)
+    rating = models.FloatField(null=True)
+    general_review = models.TextField(null=True)
+    total_review_num = models.IntegerField(null=True)
+    image_link = models.URLField(null=True)
+    img_link_2 = models.URLField(null=True)
+    img_link_3 = models.URLField(null=True)
+    img_link_4 = models.URLField(null=True)
+    img_link_5 = models.URLField(null=True)
 
     def __str__(self):
         return self.room_name
@@ -22,5 +30,13 @@ class Accomodation(models.Model):
         self.location = list[2]
         self.latitude = list[3]
         self.longitude = list[4]
-        self.link = list[5]
-        self.image_link = list[6]
+        self.accomodation_type = list[5]
+        self.min_price = list[6]
+        self.rating = list[7]
+        self.general_review = list[8]
+        self.total_review_num = list[9]
+        self.image_link = list[10]
+        self.img_link_2 = list[11]
+        self.img_link_3 = list[12]
+        self.img_link_4 = list[13]
+        self.img_link_5 = list[14]
